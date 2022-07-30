3d9a84199d3caee5984383e9725f4ee447e02b0f
연두 <108284815+yeondu1062@users.noreply.github.com>
2022-07-30 14:53:19 +0900

    Update yeondu.ts

--- yeondu.ts
+++ yeondu.ts
@@ -1,6 +1,7 @@
+const bancmd = ['list','me','msg','tell','w'];   //밴 명령어
+
 import { command } from "bdsx/command";
 
-const bancmd = ['list','me','msg','tell','w'];
 for(let i = 0; i < bancmd.length; i++) {
     command.find(bancmd[i]).signature.permissionLevel = 4;
 };
